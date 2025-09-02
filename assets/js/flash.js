f491def6df3ed0535cd921926291efd6bf11d951
Daudi Lukonge <daudilukonge@gmail.com>
2025-09-02 20:35:57 +0300

    New update

--- assets/js/flash.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-    const flashMessages = document.querySelectorAll('.flash-message');
-    flashMessages.forEach(msg => {
-        msg.style.opacity = '0';
-        setTimeout(() => msg.remove(), 500); // remove from DOM after fade out
-    }); 
-}, 5000);
